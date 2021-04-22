adfb9326709b63904d749e6fd9ff229ec6a97fcc
高若桁 <kaoronny@gmail.com>
2021-04-22 23:29:51 +0800

    new_version

--- linebot.py
+++ linebot.py
@@ -4,8 +4,6 @@ Created on Thu Apr 22 15:49:35 2021
 
 @author: logankao
 """
-import os
-from datetime import datetime
 
 from flask import Flask, abort, request
 
@@ -16,31 +14,35 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+line_bot_api = LineBotApi("5zH5Jqhngz6pNsOO2buDwZb9CiEtmsjGSqLRwiSWFZJyFdhzyophyHMIOHqkAfdE5Kb6l+MnS0eWvjFzlzAO7dFXp3s8+XOG6xxkGqk6N2MX7HmLqomAm0haADWfd1YQSz37smbB0+ijYlmNPq1/TgdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("241e28de45063bf87dd63f1b93b7f727")
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-    if request.method == "GET":
-        return "Hello Heroku"
-    if request.method == "POST":
-        signature = request.headers["X-Line-Signature"]
-        body = request.get_data(as_text=True)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-        return "OK"
+    return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+
+if __name__ == "__main__":
+    app.run()
